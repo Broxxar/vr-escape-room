8addc04f7ebb6d0176aa35c5922dcefd47ef6ae5
Demanpyro <aarweing@gmail.com>
2019-01-26 10:09:50 -0800

    audio testing
    got audio to work when you pick up an object in the scene

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private AudioSource audio;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+
+        if(this.gameObject.tag == "Patient")
+        {
+            audio.panStereo = .5f;
+        }
+        if (this.gameObject.tag == "Physician")
+        {
+            audio.panStereo = -.5f;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!rb.isKinematic)
+        {
+            audio.Play();
+        }
+		
+	}
+}

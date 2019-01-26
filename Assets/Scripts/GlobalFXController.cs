b5af19916a8d78d2ce1747f54ecec828810331d3
Broxxar <danjohnmoran@gmail.com>
2019-01-26 13:43:40 -0800

    Updates to shader global FX with transitions

--- Assets/Scripts/GlobalFXController.cs
+++ Assets/Scripts/GlobalFXController.cs
@@ -1,25 +1,93 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 [ExecuteInEditMode]
 public class GlobalFXController : MonoBehaviour
 {
     [SerializeField]
     [Range(0, 25)]
-    private float _grayscaleDistance;
-
+    private float _whiteOutDistance;
+    [SerializeField]
+    [Range(0, 25)]
+    private float _desaturationDistance;
+    [SerializeField]
+    [Range(0, 1)]
+    private float _saturationValue = 1.0f;
     [SerializeField]
     private Texture2D _noiseTexture;
 
+    [SerializeField]
+    private AnimationCurve _whiteOutCurve;
+
+    [SerializeField]
+    private AnimationCurve _desaturationCruve;
+
+    private Coroutine _activeCoroutine;
+    private float _transitionProgress;
+    private float _transitionVelocity;
+
+    private const string WhiteOutDistanceName = "_GlobalWhiteOutDistance";
     private const string DesaturationPositionName = "_GlobalDesaturationPosition";
-
+    private const string SaturationValueName = "_GlobalSaturationValue";
     private const string DesaturationDistanceName = "_GlobalDesaturationDistance";
-
     private const string DesaturationNoiseTexture = "_GlobalDesaturationNoiseTex";
+    private const string GlobalFXKeyword = "_GLOBALFX_ENABLED";
+
+    public void FadeWorldIn(Action onComplete = null)
+    {
+        if (_activeCoroutine != null)
+        {
+            StopCoroutine(_activeCoroutine);
+            _activeCoroutine = null;
+        }
+        _activeCoroutine = StartCoroutine(TransitionAsync(1.0f, onComplete));
+    }
+
+    public void FadeWorldOut(Action onComplete = null)
+    {
+        if (_activeCoroutine != null)
+        {
+            StopCoroutine(_activeCoroutine);
+            _activeCoroutine = null;
+        }
+        _activeCoroutine = StartCoroutine(TransitionAsync(0.0f, onComplete));
+    }
+
+    private IEnumerator TransitionAsync(float transitionTarget, Action onComplete)
+    {
+        var transitionTime = Mathf.Abs(transitionTarget - _transitionProgress);
+        while(Mathf.Abs(_transitionProgress - transitionTarget) > 0.05f)
+        {
+            _transitionProgress = Mathf.SmoothDamp(_transitionProgress, transitionTarget, ref _transitionVelocity, transitionTime);
+            _whiteOutDistance = _whiteOutCurve.Evaluate(_transitionProgress);
+            _desaturationDistance = _desaturationCruve.Evaluate(_transitionProgress);
+            yield return null;
+        }
+        _whiteOutDistance = _whiteOutCurve.Evaluate(transitionTarget);
+        _desaturationDistance = _desaturationCruve.Evaluate(transitionTarget);
+        if (onComplete != null)
+        {
+            onComplete.Invoke();
+        }
+    }
+
+    private void OnEnable()
+    {
+        Shader.EnableKeyword(GlobalFXKeyword);
+    }
+
+    private void OnDisable()
+    {
+        Shader.DisableKeyword(GlobalFXKeyword);
+    }
 
     private void Update()
     {
+        Shader.SetGlobalFloat(WhiteOutDistanceName, _whiteOutDistance);
         Shader.SetGlobalVector(DesaturationPositionName, transform.position);
-        Shader.SetGlobalFloat(DesaturationDistanceName, _grayscaleDistance);
+        Shader.SetGlobalFloat(DesaturationDistanceName, _desaturationDistance);
         Shader.SetGlobalTexture(DesaturationNoiseTexture, _noiseTexture);
+        Shader.SetGlobalFloat(SaturationValueName, _saturationValue);
     }
 }
--- Assets/Shaders/AnterogradeStandardShaderExtra.cginc
+++ Assets/Shaders/AnterogradeStandardShaderExtra.cginc
@@ -1,21 +1,31 @@
+half _GlobalWhiteOutDistance;
 half3 _GlobalDesaturationPosition;
 half _GlobalDesaturationDistance;
+half _GlobalSaturationValue;
 sampler2D _GlobalDesaturationNoiseTex;
 
 half4 PostProcessFragment(half4 color, half3 posWorld, half3 normalWorld)
 {
-    half lum = dot(color.rgb, half3(0.30, 0.59, 0.11));
+    #ifdef _GLOBALFX_ENABLED
+        half lum = dot(color.rgb, half3(0.30, 0.59, 0.11));
     
-    float dist = length(posWorld - _GlobalDesaturationPosition);
-    
-    float3 triW = abs(normalWorld);
-    triW /= (triW.x + triW.y + triW.z);
-    float xyNoise = tex2D(_GlobalDesaturationNoiseTex, posWorld.xy);
-    float xzNoise = tex2D(_GlobalDesaturationNoiseTex, posWorld.xz);
-    float yzNoise = tex2D(_GlobalDesaturationNoiseTex, posWorld.yz);    
-    float noise = xyNoise * triW.z + xzNoise * triW.y + yzNoise * triW.x;
-    
-    float t = saturate(smoothstep(0, _GlobalDesaturationDistance, dist - noise));
-    
-    return lerp(color, lum, t);
+        float dist = length(posWorld - _GlobalDesaturationPosition);
+        
+        float3 triW = abs(normalWorld);
+        triW /= (triW.x + triW.y + triW.z);
+        float3 noiseUV = posWorld * 0.25;
+        float xyNoise = tex2D(_GlobalDesaturationNoiseTex, noiseUV.xy);
+        float xzNoise = tex2D(_GlobalDesaturationNoiseTex, noiseUV.xz);
+        float yzNoise = tex2D(_GlobalDesaturationNoiseTex, noiseUV.yz);    
+        float noise = xyNoise * triW.z + xzNoise * triW.y + yzNoise * triW.x;
+        
+        float tDesat = saturate(smoothstep(0, _GlobalDesaturationDistance, dist - noise));
+        float tWhiteOut = saturate(smoothstep(0, _GlobalWhiteOutDistance, dist - noise));
+        
+        color = lerp(color, lum, min(1 - _GlobalSaturationValue, tDesat));
+
+        return lerp(color, 1, tWhiteOut);
+    #else
+        return color;
+    #endif
 }

2fbc9ee0663666fbac5df43343b0a3cf6eab58e1
Demanpyro <aarweing@gmail.com>
2019-01-26 18:39:40 -0800

    Puzzle work
    checking system to see whether all puzzles are complete or not

---
+++ Assets/Scripts/Puzzles/Puzzle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Puzzle : MonoBehaviour
+{
+    public class PuzzleEvent : UnityEvent<Puzzle>{ }
+
+    public PuzzleEvent onComplete = new PuzzleEvent();
+
+    public bool isComplete { get; private set; }
+
+    protected void Complete()
+    {
+        if (!isComplete) {
+            isComplete = true;
+            onComplete.Invoke(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/PuzzleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour {
+
+    public List<Puzzle> puzzles;
+
+	// Use this for initialization
+	void Start () {
+
+        foreach(var puzzle in puzzles)
+        {
+            puzzle.onComplete.AddListener(OnPuzzleCompleted);
+        }
+		
+	}
+
+    void OnPuzzleCompleted(Puzzle puzzle)
+    {
+        foreach (var p in puzzles)
+        {
+            if (!p.isComplete)
+            {
+                return;
+            }
+            //print(puzzle.name);
+        }
+        print("All puzzles complete");
+    }
+}

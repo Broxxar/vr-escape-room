7a4ca1e9f3d315beda3c6bde4f1c4f9af24c5a4a
James Lin <james97lin@gmail.com>
2019-01-27 02:04:38 -0800

    Added snap area prefab. Added generic TriggerPuzzle that goes on a trigger volume or snap area and waits for an obj w/ a certain tag to complete.

---
+++ Assets/Scripts/Puzzles/TriggerPuzzle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Waits for objs to enter a trigger volume. When an object of the appropriate tag does so,
+ * it completes the puzzle. Intended to be attached to the trigger volume object.
+ */
+public class TriggerPuzzle : Puzzle {
+
+    public string tag;
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(tag)) {
+            Complete();
+        }
+    }
+}
